b0d0f48a33e832cc0057964b5a93adc5b2dcab10
Raphaël <simonraphael@hotmail.fr>
2023-05-21 19:51:41 +0200

    update

--- ressources/credit-card-checker/script.js
+++ ressources/credit-card-checker/script.js
@@ -22,7 +22,6 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 // An array of all the arrays above
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
 
-
 // Add your functions below:
 const validateCred = array => {
   let reverseArray = array.reverse();
